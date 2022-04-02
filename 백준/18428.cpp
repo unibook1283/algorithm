1a4584da7ac5fc9f7db7a23616549e88b857f5a7
unibook1283 <uni_book@naver.com>
2022-04-02 22:58:56 +0900

    1문제

---
+++ 백준/18428.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+char a[6][6];
+vector<pair<int, int>> x, t;
+int n;
+int dx[] = { 0,0,1,-1 };
+int dy[] = { 1,-1,0,0 };
+bool can_avoid() {
+	for (int i = 0; i < t.size(); i++) {
+		int tx = t[i].first;
+		int ty = t[i].second;
+		for (int j = 0; j < 4; j++) {
+			int nx = tx;
+			int ny = ty;
+			while (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+				if (a[nx][ny] == 'S') return false;
+				if (a[nx][ny] == 'O') break;
+				nx += dx[j];
+				ny += dy[j];
+			}
+		}
+	}
+	return true;
+}
+void go(int cnt, int index) {
+	if (cnt == 3) {		// 3개를 정한 경우
+		if (!can_avoid()) {
+			return;
+		}
+		cout << "YES";
+		exit(0);
+	}
+	/*
+	if (index == x.size()) {	// 다 돌았는데 3개를 못 정한 경우를 처리 안해줘도 되네?
+
+	}
+	*/
+	for (int i = index; i < x.size(); i++) {
+		a[x[i].first][x[i].second] = 'O';
+		go(cnt + 1, index + 1);
+		a[x[i].first][x[i].second] = 'X';
+	}
+}
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
+			if (a[i][j] == 'T') {
+				t.push_back(make_pair(i, j));
+			}
+			if (a[i][j] == 'X') {
+				x.push_back(make_pair(i,j));
+			}
+		}
+	}
+	go(0, 0);
+	cout << "NO";
+
+	return 0;
+}
