213f525e5bb609d0aee2aee1b44419281c18961e
unibook1283 <uni_book@naver.com>
2022-04-08 19:13:09 +0900

    다이나믹 1문제

---
+++ 백준/14494.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+long long d[1001][1001];
+int main() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		d[i][1] = 1;
+	}
+	for (int i = 1; i <= m; i++) {
+		d[1][i] = 1;
+	}
+	for (int i = 2; i <= n; i++) {
+		for (int j = 2; j <= m; j++) {
+			d[i][j] = d[i - 1][j] + d[i - 1][j - 1] + d[i][j - 1];
+			d[i][j] %= 1000000007;
+		}
+	}
+	cout << d[n][m] << '\n';
+
+	return 0;
+}
