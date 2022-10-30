6e6c386d7528136d7c9bfd31e09b2af8e6543139
unibook1283 <uni_book@naver.com>
2022-10-31 08:25:52 +0900

    1문제

---
+++ 알고스팟/wildcard(3).cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+int cache[101][101];
+string W, S;
+bool match(int w, int s) {
+	int& ret = cache[w][s];
+	if (ret != -1) return ret;
+	while (s < S.size() && w < W.size() &&
+		(W[w] == '?' || W[w] == S[s])) {
+		w++;
+		s++;
+	}
+	if (w == W.size())
+		return ret = (s == S.size());
+	if (W[w]=='*')
+		for (int skip = 0; s + skip <= S.size(); skip++)
+			if (match(w + 1, s + skip))
+				return ret = 1;
+	return ret = 0;
+}
+int main() {
+	int t;
+	cin >> t;
+	while (t--) {
+		cin >> W;
+		int n;
+		cin >> n;
+		vector<string> v;
+		for (int i = 0; i < n; i++) {
+			memset(cache, -1, sizeof(cache));
+			cin >> S;
+			if (match(0, 0)) {
+				v.push_back(S);
+			}	
+		}
+		sort(v.begin(), v.end());
+		for (int i = 0; i < v.size(); i++) {
+			cout << v[i] << '\n';
+		}
+	}
+	return 0;
+}
